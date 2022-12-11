33abbe5b450f169cba5767ecc63ef8625399682b
dell <dell@192.168.2.196>
2022-12-11 14:05:35 +0900

    coment

--- springmvc_mybatis_db/src/cn/itcast/ssm/controller/LoginController.java
+++ springmvc_mybatis_db/src/cn/itcast/ssm/controller/LoginController.java
@@ -14,6 +14,7 @@ import cn.itcast.ssm.common.str.MyDateUtil;
 import cn.itcast.ssm.po.custom.CustomUser;
 import cn.itcast.ssm.po.original.TUser;
 import cn.itcast.ssm.service.UserService;
+import cn.itcast.ssm.vo.LoginBean;
 
 @Controller
 public class LoginController {
@@ -24,19 +25,26 @@ public class LoginController {
 
 	// 登陆
 	@RequestMapping("/login")
-	public String login(Model model, HttpSession session, String username, String password)
+	public String login(Model model, HttpSession session, LoginBean loginBean )
 			throws Exception {
-
-	    if ((username == null || username.length() == 0)
-	            && (password == null || password.length() == 0)
+        if(loginBean.getWord() == null || loginBean.getWord().length()==0) {
+		    model.addAttribute("usernamexx", loginBean.getUsername());
+		    model.addAttribute("passwordyy",loginBean.getPassword());
+		    model.addAttribute("loginInfo",loginBean);
+		    
+        	model.addAttribute("error", "没有输入验证码");
+        	return "login";
+        }
+	    if ((loginBean.getUsername() == null || loginBean.getUsername().length() == 0)
+	            && (loginBean.getPassword() == null || loginBean.getPassword().length() == 0)
 	            ) {
 	        return "login";
         }
 		// 调用service进行用户身份验证
-		TUser user = userService.findUser(username);
-		if (user == null || !password.equals(user.getPassword())) {
+		TUser user = userService.findUser(loginBean.getUsername());
+		if (user == null || !loginBean.getPassword().equals(user.getPassword())) {
 
-		    model.addAttribute("username", username);
+
 			model.addAttribute("error", "用户名和密码不一致");
 		     // 清除session
 	        session.invalidate();
@@ -45,12 +53,12 @@ public class LoginController {
 
 
 		// 在session中保存用户身份信息
-		session.setAttribute("username", username);
+		session.setAttribute("username", loginBean.getUsername());
 		// 重定向到商品列表页面
 		return "redirect:/items/initItems.action";
 	}
 
-
+	
 
 	// 登陆
 	@RequestMapping("register")
---
+++ springmvc_mybatis_db/src/cn/itcast/ssm/vo/LoginBean.java
@@ -0,0 +1,26 @@
+package cn.itcast.ssm.vo;
+
+public class LoginBean {
+	private String username;
+	private String password;
+	private String word;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+}
