1bc211efc30f2c15dd8de25c8619b7b50d322ba5
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-01-14 18:25:06 +0900

    html02

---
+++ springmvc_mybatis_db/src/jp/co/yahoo/hw/ym202301/saisk/test/basic/HelloWorld.java
@@ -0,0 +1,16 @@
+package jp.co.yahoo.hw.ym202301.saisk.test.basic;
+//包名，一般是公司网站的反向写法。
+//该文件所在的文件夹路径
+
+public class HelloWorld {
+
+	// 输入main     代码补全快捷键 ctrl+space 或 alt +／
+	
+	// 程序入口
+	public static void main(String[] args) {
+		// syso 
+		System.out.println("hello world1");
+
+	}
+
+}
