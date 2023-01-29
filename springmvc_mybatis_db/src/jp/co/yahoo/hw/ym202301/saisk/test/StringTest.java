56ea74eef28f6e624d1822c2367ecbe0d6dbf7af
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-01-29 13:35:50 +0900

    aaa

---
+++ springmvc_mybatis_db/src/jp/co/yahoo/hw/ym202301/saisk/test/StringTest.java
@@ -0,0 +1,26 @@
+package jp.co.yahoo.hw.ym202301.saisk.test;
+
+public class StringTest {
+
+//	求一个方法isSymmetry(String str)
+//	如果是abccba  返回true
+//	如果是abcscba 返回true
+//	如果是abcsbba 返回false
+
+	
+	public static void main(String[] args) {
+		
+		String str="abccba";
+		System.out.println(str.contains("abc"));
+		
+		
+		
+//		System.out.println(isSymmetry("abccba"));
+	}
+
+	private static int add(int i, int j) {
+		return i+j;
+	}
+	
+	
+}
