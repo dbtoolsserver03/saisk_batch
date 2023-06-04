a331158f398709c8dc0601e0be0384b90d6cfdf6
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-06-04 16:01:18 +0900

    validate word

--- springmvc_mybatis_db/src/cn/itcast/ssm/vo/LoginBean.java
+++ springmvc_mybatis_db/src/cn/itcast/ssm/vo/LoginBean.java
@@ -3,9 +3,16 @@ package cn.itcast.ssm.vo;
 public class LoginBean {
 
 	// 变量名一定要和JSP里面的name相同
-	 private String username; 
+	 private String username;
 	 private String password;
-	 
+	 private String validateWord;
+
+	public String getValidateWord() {
+		return validateWord;
+	}
+	public void setValidateWord(String validateWord) {
+		this.validateWord = validateWord;
+	}
 	public String getUsername() {
 		return username;
 	}
@@ -18,5 +25,5 @@ public class LoginBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	 
+
 }
