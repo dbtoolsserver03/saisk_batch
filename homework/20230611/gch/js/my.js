1dcedb3087bb1ae637aa6a00bedb7c4c97cadff1
gu-m2 <gu-m2@localhost>
2023-06-25 07:51:34 +0900

    homework

---
+++ homework/20230611/gch/js/my.js
@@ -0,0 +1,53 @@
+
+		// 下面的写法是错的，因为BODY里面的组件还没有完全加载完毕。
+		// document.getElementById("id_user");
+
+		alert(hello);
+		var  newTitle = "xxx"
+		document.title = newTitle;
+
+			function funLogin(obj,num) {
+				alert(obj.value + num);
+
+				var element = document.getElementById("id_user");
+				element.style.backgroundColor="green";
+
+				var elementPw = document.getElementById("id_pw");
+
+				var id_view_element = document.getElementById("id_view");
+
+				var sexElements=document.getElementsByName("sex");
+				var sexVal="";
+				for(var i=0; i<sexElements.length; i++){
+					if(sexElements[i].checked) {
+						sexVal = sexElements[i].value;
+						break;
+					}
+				}
+				// id_view_element.innerText=element.value +"\n"+ sexVal;
+				id_view_element.innerHTML=element.value +"<br>"+ elementPw.value +"<br>" + sexVal +"<br>" ;
+
+				//element.disabled=true;
+			}
+
+			// 1.HTML里，增加一个按钮，在这个按钮上加事件
+			// 2.HTML里，实现这个事件对应的方法
+			// 3.JS里，
+			function funClear() {
+
+				// 从当前HTML文件里取指定ID的标签
+				// document 相当于这个HTML
+				// getElementById  从这个document里面取Id 是 "id_user" 的标签
+
+				// var 定义变量
+				// (JS) var a  = (java) int a
+				// var 是无类型
+
+				var element = document.getElementById("id_user");
+				element.value="";
+			}
+
+			function init() {
+				alert(document.getElementById("id_user").value);
+				
+			}
